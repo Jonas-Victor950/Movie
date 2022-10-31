9d3f7f71ef07727541afb517b9c8889e63f4e488
Jonas-Victor950 <111525829+Jonas-Victor950@users.noreply.github.com>
2022-10-31 15:51:48 -0300

    Integrating Winston

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,8 @@
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASS
+
 export default {
     port: 3000,
-    dbUri: "mongodb+srv://jonas:QqDlTwVmQLu0GmjJ@cluster0.pjcoxhe.mongodb.net/?retryWrites=true&w=majority"
+    dbUri: `mongodb+srv://${dbUser}:${dbPassword}@cluster0.pjcoxhe.mongodb.net/?retryWrites=true&w=majority`,
+    env: "development"
 }
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,7 @@
+
+// ENV variable
+require("dotenv").config()
+
 import express from "express"
 import config from "config"
 
@@ -11,13 +15,16 @@ import db from "../config/db"
 
 // Routes 
 import router from "./router"
-
+// Logger
+import Logger from "../config/logger";
 app.use("/api/", router)
+
+
 
 // app port
 const port = config.get<number>("port")
 
 app.listen(3000, async () => {
     await db()
-    console.log(`Aplicação está funcionando na porta: ${port}`)
+    Logger.info(`Aplicação está funcionando na porta: ${port}`)
 })
